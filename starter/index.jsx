c6df4ee19281dbd4f6768289dc3137138e1a982b
anton99999 <an280502@gmail.com>
2023-07-26 17:00:56 +0300

    wolt 1.2.0

--- starter/index.jsx
+++ starter/index.jsx
@@ -5,7 +5,7 @@ let docs_link = "https://github.com/ghtx280/wolt";
 
 <div class="h-100vh bg-#222" flex="40 col center">
   <h1 text="orange">
-    `Wellcome to { app_name }`
+    `Welcome to { app_name }`
   </h1>
   
   <a href="{docs_link}" text="20 #fff line">{docs_label}</a>
